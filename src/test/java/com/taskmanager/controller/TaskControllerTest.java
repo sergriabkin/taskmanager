153c31afa50040a09dea6c0614c506252ff5b4ce
Serhii_Riabkin <serhii_riabkin@epam.com>
2020-02-22 09:16:39 +0200

    project skeleton creation : getOne and getAll controller test

--- src/test/java/com/taskmanager/controller/TaskControllerTest.java
+++ src/test/java/com/taskmanager/controller/TaskControllerTest.java
@@ -1,30 +1,61 @@
 package com.taskmanager.controller;
 
+import com.taskmanager.model.Task;
+import com.taskmanager.service.TaskService;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(TaskController.class)
 class TaskControllerTest {
 
+    @MockBean
+    private TaskService service;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     void addTask() {
+
     }
 
     @Test
-    void getAll() {
+    void getAll() throws Exception {
+        List<Task> mockTasksList = new ArrayList<>();
+        mockTasksList.add(new Task("TestTitle1", "TestDescription1"));
+        mockTasksList.add(new Task("TestTitle2", "TestDescription2"));
+        BDDMockito.given(service.getAll()).willReturn(mockTasksList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/tasks"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("TestTitle1")))
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("TestTitle2")))
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("TestDescription1")))
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("TestDescription2")));
     }
 
     @Test
-    void getOne() {
+    void getOne() throws Exception {
+        Task mockTask = new Task("TestTitle1", "TestDescription1");
+        BDDMockito.given(service.getOne(100L)).willReturn(mockTask);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/tasks/100"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("TestTitle1")))
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("TestDescription1")));
     }
 
     @Test
