4b8c870d221dfe6060a84aa3c86acf547c57b0cf
Serhii_Riabkin <serhii_riabkin@epam.com>
2020-02-24 17:18:03 +0200

    project skeleton creation : set hal-browser as a start page

--- src/main/java/com/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/taskmanager/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.taskmanager.repository;
 
 import com.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
