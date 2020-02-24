ecf7f5b7e2d6588111441651af12ab6f3265109b
Serhii_Riabkin <serhii_riabkin@epam.com>
2020-02-24 08:42:48 +0200

    project skeleton creation : add Column annotation

--- src/main/java/com/taskmanager/model/Task.java
+++ src/main/java/com/taskmanager/model/Task.java
@@ -2,6 +2,7 @@ package com.taskmanager.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,8 +10,14 @@ import javax.persistence.Id;
 @Data
 @Entity
 public class Task {
-    private @Id @GeneratedValue Long id;
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column
     private String title;
+
+    @Column
     private String description;
 
     public Task() {
