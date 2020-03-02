ed2f70f461e7962b6c8e508fc4c99eef72536499
Serhii_Riabkin <serhii_riabkin@epam.com>
2020-03-02 09:53:28 +0200

    security : user service and controller + JwtProvider

--- src/main/java/com/taskmanager/repository/UserRepository.java
+++ src/main/java/com/taskmanager/repository/UserRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
-/**
- * Created by Mary Ellen Bowman.
- */
 @RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String userName);
